333b465bb8e296ea77aaca954d108ae1cd953de9
Manou1993 <manolotsoadaniel@gmail.com>
2019-10-16 11:19:24 +0300

    ajout du fichier app_3.rb contenant le jeu version 3.0 + explication du code

--- app_3.rb
+++ app_3.rb
@@ -13,12 +13,18 @@ print "Veuillez entrer le nom de votre hero\n>"
 name = gets.chomp
 my_game = Game.new(name)
 
+# Tant que le personnage utilisateur est superieur strict à 0 et il a encore des ennemies
 while my_game.human_player.life_points > 0 && (my_game.enemies.size > 0)
+  # afficher menu
   my_game.menu
+  # demande à l'utilisateur de choisir
   variable_choice = gets.chomp
+  # executer le choix de 'l'utilisateur
   my_game.menu_choice(variable_choice)
+  # attaques des ennemies vivant
   my_game.enemies_attack
 end
+# fin du jeu
 my_game.endgame
 
 binding.pry
